b51363b96f448b2ca171da38100e99b029566f9a
Ira <perekrestova.i@yandex.ru>
2021-04-16 21:14:55 +0300

    новые страницы

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ def index():
 def register():
     main_style = url_for('static', filename='css/main_style.css')
     button_style2 = url_for('static', filename='css/button_style.css')
-    text_style = url_for('static', filename='css/text_style.css')
+    text_style2 = url_for('static', filename='css/text_style2.css')
     an_style = url_for('static', filename='css/an_style.css')
     checkbox_style = url_for('static', filename='css/checkbox_style.css')
     radio_checkbox_style = url_for('static', filename='css/radio_checkbox_style.css')
     if request.method == 'GET':
 
-        return render_template('register_template.html', main_style=main_style, text_style=text_style, an_style=an_style, button_style2=button_style2, checkbox_style=checkbox_style, radio_checkbox_style=radio_checkbox_style)
+        return render_template('register_template.html', main_style=main_style, text_style2=text_style2, an_style=an_style, button_style2=button_style2, checkbox_style=checkbox_style, radio_checkbox_style=radio_checkbox_style)
     else:
         print(request.form)
-        return redirect('/me')
+        return redirect('/choice')
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -46,18 +46,53 @@ def login():
                            radio_checkbox_style=radio_checkbox_style)
     else:
         print(request.form)
-        return redirect('/me')
+        return redirect('/choice')
 
 
-@app.route('/me')
+@app.route('/me', methods=['POST', 'GET'])
 def profile():
     main_style = url_for('static', filename='css/main_style.css')
     an_style = url_for('static', filename='css/an_style.css')
     ph_style = url_for('static', filename='css/ph_style.css')
+    text_style2 = url_for('static', filename='css/text_style2.css')
+    button_style2 = url_for('static', filename='css/button_style2.css')
+
     if request.method == 'GET':
-        return render_template('profile_template.html', main_style=main_style, an_style=an_style, ph_style=ph_style)
+        return render_template('profile_template.html', main_style=main_style, an_style=an_style, ph_style=ph_style, text_style2=text_style2, button_style2=button_style2)
     else:
-        return "hi"
+        return redirect('/changing_profile')
+
+
+@app.route('/choice')
+def choice():
+    main_style2 = url_for('static', filename='css/main_style2.css')
+    button_style2 = url_for('static', filename='css/button_style2.css')
+    return render_template('after_reg.html', button_style2=button_style2, main_style2=main_style2)
+
+
+@app.route('/people')
+def people():
+    main_style = url_for('static', filename='css/main_style.css')
+
+    return render_template('people_temp.html',  main_style=main_style)
+
+
+@app.route('/changing_profile', methods=['POST', 'GET'])
+def changing():
+    main_style = url_for('static', filename='css/main_style.css')
+    button_style2 = url_for('static', filename='css/button_style.css')
+    text_style2 = url_for('static', filename='css/text_style2.css')
+    an_style = url_for('static', filename='css/an_style.css')
+    checkbox_style = url_for('static', filename='css/checkbox_style.css')
+    radio_checkbox_style = url_for('static', filename='css/radio_checkbox_style.css')
+    if request.method == 'GET':
+
+        return render_template('changing_profile.html', main_style=main_style, text_style2=text_style2,
+                               an_style=an_style, button_style2=button_style2, checkbox_style=checkbox_style,
+                               radio_checkbox_style=radio_checkbox_style)
+    else:
+        print(request.form)
+        return redirect('/choice')
 
 
 if __name__ == '__main__':
---
+++ static/css/login_form.py
@@ -0,0 +1,14 @@
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
